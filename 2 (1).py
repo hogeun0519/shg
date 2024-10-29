5cfd440a28f725d42c74f51bf7755b0fe4226ea9
hogeun0519 <163367456+hogeun0519@users.noreply.github.com>
2024-10-29 14:48:03 +0900

    Update 2 (1).py

--- 2 (1).py
+++ 2 (1).py
@@ -101,6 +101,54 @@ class Optimizer_SGD:
     def post_update_params(self):
         self.iterations += 1
 
+class Optimizer_Adagrad:
+
+    def__init__(self,learning_rating = 1., decay=0., epsilon=1e-7);
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+
+
+def__init__(self,learning_rating = 1., decay=0., epsilon=1e-7):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self. learning_rate * (1. / 1. + self.decay * self.iterations)
+    def update_params(self,layer):
+        if not hasattr(layer, 'weighted_cache'):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+    layer.weight_cache += layer.dweights ** 2
+    layer.bias_cache += layer.dbiases ** 2
+
+    layer.weight = -self.current_rate * layer.dweights\
+                    / (np.sqrt(layer.weight_cache) + self.epsilon
+    layer.biases = -self.current_rate * layer.dbiases \
+                            /(np.sqrt(layer.bias_cache)+ self.epsilon)
+
+    def post_update_params(self):
+        self.iterations += 1
+
+class Optimizer_RMSprop:
+
+    def__init__(self,learning_rating = 1., decay=0., epsilon=1e-7);
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+
+    layer.weight_cache = self.rho * layer.weight_cache +\
+                        /layer.dweights ** 2
+    layer.bias_cache += layer.dbiases ** 2
 
 class Loss:
 
